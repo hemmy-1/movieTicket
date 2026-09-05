4f142060aad7fadb60f0fef1fa4d6ada1e6b15ca
Adegbola Emmanuel <princeadekunle2001@gmail.com>
2026-09-05 01:31:26 +0100

    setup the configuration

---
+++ src/main/java/com/example/movieTicket/configuration/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.example.movieTicket.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.example.movieTicket.filter.JwtFilter;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private JwtFilter jwtFilter;
+
+    public SecurityConfig(JwtFilter jwtFilter) {
+        this.jwtFilter = jwtFilter;
+    }
+
+    // Cross Site Request Forgery
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(
+                        auth -> auth.requestMatchers("/auth/**")
+                                .permitAll()
+                                .anyRequest().authenticated())
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+}
